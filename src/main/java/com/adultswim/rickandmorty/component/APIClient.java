4cce495ec11224a20690b73aa2ccf4a4092662b5
tomas <tomas.pascual@wenance.com>
2021-03-24 11:17:48 +0100

    fixing an issue with WebClient SSL context: It wasn't understanding the correct protocols and ciphers

--- src/main/java/com/adultswim/rickandmorty/component/APIClient.java
+++ src/main/java/com/adultswim/rickandmorty/component/APIClient.java
@@ -1,9 +1,6 @@
 package com.adultswim.rickandmorty.component;
 
-import com.adultswim.rickandmorty.entity.Characters;
 import com.adultswim.rickandmorty.entity.SpecificCharacters;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.channel.ChannelOption;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
@@ -12,34 +9,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.http.codec.ClientCodecConfigurer;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
 import javax.net.ssl.SSLException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.*;
-
+import java.util.List;
+import java.util.Set;
 
 
 @Component
@@ -73,18 +52,27 @@ public class APIClient {
             }
         }
 
-        logger.info("PATH: " + String.join(",", charactersList));
+        Iterable<String> allowedCiphers = List.of("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384");
 
+
+        // Webclient that trusts any external SSL certificate
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .protocols("SSLv3","TLSv1","TLSv1.1","TLSv1.2")
+                .ciphers(allowedCiphers)
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(ssl -> { ssl.sslContext(sslContext); });
+        ClientHttpConnector httpConnector = new ReactorClientHttpConnector(httpClient);
         WebClient wc =  WebClient
                 .builder()
                 .baseUrl(charactersUrl + "/" + String.join(",", charactersList))
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .defaultHeader(HttpHeaders.ACCEPT_CHARSET, MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .exchangeStrategies(ExchangeStrategies.builder().codecs(this::acceptedCodecs).build())
+                .clientConnector(httpConnector)
                 .build();
 
         return wc
                 .get()
+                //.accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToFlux(SpecificCharacters.class)
                 .map(specificCharacters -> {
@@ -98,12 +86,6 @@ public class APIClient {
                 })
                 .collectSortedList(popularityComparator.reversed()); // The Comparator will order based in the popularity index declared inside
 
-    }
-
-
-    private void acceptedCodecs(ClientCodecConfigurer clientCodecConfigurer) {
-        clientCodecConfigurer.customCodecs().register(new Jackson2JsonEncoder(new ObjectMapper(), MediaType.APPLICATION_JSON));
-        clientCodecConfigurer.customCodecs().register(new Jackson2JsonDecoder(new ObjectMapper(), MediaType.TEXT_PLAIN));
     }
 
 
